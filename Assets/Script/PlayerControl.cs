25b78723c71004d4c7ec1733cb23591e0f0ada6f
bewether <150963108+bewether@users.noreply.github.com>
2024-07-01 21:39:41 +0300

    Test2

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed = 5f; // �������� �������� ���������
+    public float jumpForce = 5f; // ���� ������
+
+    private bool isGrounded; // �������� ��������� �� �������� �� �����
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // ����� ����� ���� ��� ��� ��������� ������
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
