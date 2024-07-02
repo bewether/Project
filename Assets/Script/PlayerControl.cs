a04a725278233ccc4adebde6a7eee46b0a3766cb
bewether <150963108+bewether@users.noreply.github.com>
2024-07-02 17:54:32 +0300

    Update 02.07.2024

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -4,41 +4,44 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float moveSpeed = 5f; // �������� �������� ���������
-    public float jumpForce = 5f; // ���� ������
+    public float moveSpeed = 5f; // скорость движения персонажа
+    public float jumpForce = 5f; // сила прыжка
 
-    private bool isGrounded; // �������� ��������� �� �������� �� �����
+    private bool isGrounded; // проверка находится ли персонаж на земле
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         transform.position += movement * Time.deltaTime * moveSpeed;
 
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            GetComponent<Rigidbody>().AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
         }
     }
 
-    void FixedUpdate()
-    {
-        // ����� ����� ���� ��� ��� ��������� ������
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Ground")
         {
             isGrounded = true;
         }
     }
-
-    void OnCollisionExit2D(Collision2D collision)
+    void OnCollisionExit(Collision collision)
     {
         if (collision.collider.tag == "Ground")
         {
             isGrounded = false;
+        }
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        // Проверяем, не столкнулись ли мы со стеной
+        if (collision.collider.tag == "Wall")
+        {
+            // Останавливаем движение персонажа
+            GetComponent<Rigidbody>().velocity = new Vector3(0f, GetComponent<Rigidbody>().velocity.y, 0f);
         }
     }
 }
